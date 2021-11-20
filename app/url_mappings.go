56d4858f4995ac4e5932a1b464eacbb36ceef934
johannes-kuhfuss <johannes.kuhfuss@web.de>
2021-11-20 16:04:23 +0100

    Renamed controller to handler

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,15 +1,15 @@
 package app
 
 import (
-	"github.com/johannes-kuhfuss/probesvc/controller"
 	"github.com/johannes-kuhfuss/probesvc/domain"
+	"github.com/johannes-kuhfuss/probesvc/handler"
 	"github.com/johannes-kuhfuss/probesvc/service"
 	"github.com/johannes-kuhfuss/services_utils/logger"
 )
 
 func mapUrls() {
 	logger.Debug("Mapping URLs")
-	jh := controller.JobHandlers{Service: service.NewJobService(domain.NewJobRepositoryDb())}
+	jh := handler.JobHandlers{Service: service.NewJobService(domain.NewJobRepositoryDb())}
 	router.GET("/jobs", jh.GetAllJobs)
 	router.GET("jobs/:job_id", jh.GetJobById)
 	logger.Debug("Done mapping URLs")

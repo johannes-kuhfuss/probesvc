51d124a02a517958dbfc473aa17befb85d6ede65
johannes-kuhfuss <johannes.kuhfuss@web.de>
2021-11-21 11:09:56 +0100

    refactored dbclient, wire app

--- app/application.go
+++ app/application.go
@@ -2,24 +2,47 @@ package app
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 	"github.com/johannes-kuhfuss/probesvc/config"
+	"github.com/johannes-kuhfuss/probesvc/domain"
+	"github.com/johannes-kuhfuss/probesvc/handler"
+	"github.com/johannes-kuhfuss/probesvc/service"
 	"github.com/johannes-kuhfuss/services_utils/logger"
 )
 
 var (
-	router *gin.Engine
+	router     *gin.Engine
+	dbClient   *sqlx.DB
+	jobHandler handler.JobHandlers
 )
 
-func init() {
-	logger.Debug("Initializing router")
+func getDbClient() *sqlx.DB {
+	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", config.DbUser, config.DbPasswd, config.DbAddr, config.DbPort, config.DbName)
+	client, err := sqlx.Open("mysql", dataSource)
+	if err != nil {
+		panic(err)
+	}
+	client.SetConnMaxLifetime(time.Minute * 3)
+	client.SetMaxOpenConns(10)
+	client.SetMaxIdleConns(10)
+	return client
+}
+
+func initRouter() {
 	gin.SetMode(config.GinMode)
 	gin.DefaultWriter = logger.GetLogger()
 	router = gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
-	logger.Debug("Done initializing router")
+}
+
+func wireApp() {
+	dbClient := getDbClient()
+	customerRepositoryDb := domain.NewJobRepositoryDb(dbClient)
+	jobHandler = handler.JobHandlers{Service: service.NewJobService(customerRepositoryDb)}
 }
 
 func startRouter() {
@@ -33,6 +56,8 @@ func startRouter() {
 
 func StartApp() {
 	logger.Info("Starting application")
+	initRouter()
+	wireApp()
 	mapUrls()
 	startRouter()
 	logger.Info("Application ended")
--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,34 +1,12 @@
 package app
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/jmoiron/sqlx"
-	"github.com/johannes-kuhfuss/probesvc/config"
-	"github.com/johannes-kuhfuss/probesvc/domain"
-	"github.com/johannes-kuhfuss/probesvc/handler"
-	"github.com/johannes-kuhfuss/probesvc/service"
 	"github.com/johannes-kuhfuss/services_utils/logger"
 )
 
-func getDbClient() *sqlx.DB {
-	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", config.DbUser, config.DbPasswd, config.DbAddr, config.DbPort, config.DbName)
-	client, err := sqlx.Open("mysql", dataSource)
-	if err != nil {
-		panic(err)
-	}
-	client.SetConnMaxLifetime(time.Minute * 3)
-	client.SetMaxOpenConns(10)
-	client.SetMaxIdleConns(10)
-	return client
-}
-
 func mapUrls() {
 	logger.Debug("Mapping URLs")
-	dbclient := getDbClient()
-	jh := handler.JobHandlers{Service: service.NewJobService(domain.NewJobRepositoryDb(dbclient))}
-	router.GET("/jobs", jh.GetAllJobs)
-	router.GET("jobs/:job_id", jh.GetJobById)
+	router.GET("/jobs", jobHandler.GetAllJobs)
+	router.GET("jobs/:job_id", jobHandler.GetJobById)
 	logger.Debug("Done mapping URLs")
 }

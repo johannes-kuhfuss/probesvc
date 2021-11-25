e439058306589e8bdfc3b0057fc3b03f49c2f28e
johannes-kuhfuss <johannes.kuhfuss@web.de>
2021-11-25 08:45:03 +0100

    continued unit tests

--- handler/JobHandler_test.go
+++ handler/JobHandler_test.go
@@ -2,9 +2,11 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
@@ -33,7 +35,7 @@ func Test_getJobId_NonKsuid_Returns_BadRequestError(t *testing.T) {
 }
 
 func Test_getJobId_WithKsuid_Returns_String(t *testing.T) {
-	testParam, _ := ksuid.NewRandom()
+	testParam := ksuid.New()
 	jobId, err := getJobId(testParam.String())
 	assert.NotNil(t, jobId)
 	assert.Nil(t, err)
@@ -56,12 +58,12 @@ func Test_GetAllJobs_Returns_NoError(t *testing.T) {
 	teardown := setupTest(t)
 	defer teardown()
 	dummyJobList := createDummyJobList()
+	dummyJobListJson, _ := json.Marshal(dummyJobList)
 	mockService.EXPECT().GetAllJobs("").Return(&dummyJobList, nil)
 	router.GET("/jobs", jh.GetAllJobs)
 	request, _ := http.NewRequest(http.MethodGet, "/jobs", nil)
 
 	router.ServeHTTP(recorder, request)
-	dummyJobListJson, _ := json.Marshal(dummyJobList)
 
 	assert.EqualValues(t, http.StatusOK, recorder.Code)
 	assert.EqualValues(t, dummyJobListJson, recorder.Body.String())
@@ -82,12 +84,85 @@ func Test_GetAllJobs_Returns_Error(t *testing.T) {
 	teardown := setupTest(t)
 	defer teardown()
 	apiError := api_error.NewBadRequestError("database error")
+	errorJson, _ := json.Marshal(apiError)
 	mockService.EXPECT().GetAllJobs("").Return(nil, apiError)
 	router.GET("/jobs", jh.GetAllJobs)
 	request, _ := http.NewRequest(http.MethodGet, "/jobs", nil)
 
 	router.ServeHTTP(recorder, request)
+
+	assert.EqualValues(t, http.StatusBadRequest, recorder.Code)
+	assert.EqualValues(t, errorJson, recorder.Body.String())
+}
+
+/*
+func Test_GetJobById_Returns_InvalidIdError(t *testing.T) {
+	teardown := setupTest(t)
+	defer teardown()
+	apiError := api_error.NewBadRequestError("User id should be a ksuid")
 	errorJson, _ := json.Marshal(apiError)
+	router.GET("/jobs/:job_id", jh.GetJobById)
+	request, _ := http.NewRequest(http.MethodGet, "/jobs/:jod_id", nil)
+
+	router.ServeHTTP(recorder, request)
+
+	assert.EqualValues(t, http.StatusBadRequest, recorder.Code)
+	assert.EqualValues(t, errorJson, recorder.Body.String())
+}
+*/
+
+func Test_GetJobById_Returns_NotFoundError(t *testing.T) {
+	teardown := setupTest(t)
+	defer teardown()
+	id := ksuid.New()
+	apiError := api_error.NewNotFoundError(fmt.Sprintf("job with id %v not found", id))
+	errorJson, _ := json.Marshal(apiError)
+	mockService.EXPECT().GetJobById(gomock.Eq(id.String())).Return(nil, apiError)
+	router.GET("/jobs/:job_id", jh.GetJobById)
+	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/jobs/%v", id), nil)
+
+	router.ServeHTTP(recorder, request)
+
+	assert.EqualValues(t, http.StatusNotFound, recorder.Code)
+	assert.EqualValues(t, errorJson, recorder.Body.String())
+}
+
+func Test_GetJobById_Returns_NoError(t *testing.T) {
+	teardown := setupTest(t)
+	defer teardown()
+	id := ksuid.New()
+	newJob := dto.JobResponse{
+		Id:         id.String(),
+		Name:       "",
+		CreatedAt:  time.Time{},
+		CreatedBy:  "",
+		ModifiedAt: time.Time{},
+		ModifiedBy: "",
+		SrcUrl:     "",
+		Status:     "",
+		ErrorMsg:   "",
+		TechInfo:   "",
+	}
+	bodyJson, _ := json.Marshal(newJob)
+	mockService.EXPECT().GetJobById(gomock.Eq(id.String())).Return(&newJob, nil)
+	router.GET("/jobs/:job_id", jh.GetJobById)
+	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/jobs/%v", id), nil)
+
+	router.ServeHTTP(recorder, request)
+
+	assert.EqualValues(t, http.StatusOK, recorder.Code)
+	assert.EqualValues(t, bodyJson, recorder.Body.String())
+}
+
+func Test_CreateNewJob_Returns_InvalidJsonError(t *testing.T) {
+	teardown := setupTest(t)
+	defer teardown()
+	apiError := api_error.NewBadRequestError("invalid json body")
+	errorJson, _ := json.Marshal(apiError)
+	router.POST("/jobs", jh.CreateNewJob)
+	request, _ := http.NewRequest(http.MethodPost, "/jobs", nil)
+
+	router.ServeHTTP(recorder, request)
 
 	assert.EqualValues(t, http.StatusBadRequest, recorder.Code)
 	assert.EqualValues(t, errorJson, recorder.Body.String())

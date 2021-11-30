fb5c48f4a58be3c7e69137b6de0943f72cae6b77
johannes-kuhfuss <johannes.kuhfuss@web.de>
2021-11-30 11:27:11 +0100

    init of FileRepo added

--- app/application.go
+++ app/application.go
@@ -2,19 +2,45 @@ package app
 
 import (
 	"fmt"
+	"net/url"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 	"github.com/gin-gonic/gin"
 	"github.com/johannes-kuhfuss/probesvc/config"
 	"github.com/johannes-kuhfuss/probesvc/domain"
 	"github.com/johannes-kuhfuss/probesvc/handler"
 	"github.com/johannes-kuhfuss/probesvc/service"
+	"github.com/johannes-kuhfuss/services_utils/api_error"
 	"github.com/johannes-kuhfuss/services_utils/logger"
 )
 
 var (
-	router     *gin.Engine
-	jobHandler handler.JobHandlers
+	router      *gin.Engine
+	jobHandler  handler.JobHandlers
+	azureClient *azblob.ServiceClient
 )
+
+func connectToAzureBlob() (*azblob.ServiceClient, api_error.ApiErr) {
+	blobUrl, err := url.Parse(config.StorageBaseUrl)
+	if err != nil {
+		logger.Error("Cannot parse storage base URL", nil)
+		return nil, api_error.NewBadRequestError("Cannot parse storage base URL")
+	}
+
+	cred, err := azblob.NewSharedKeyCredential(config.StorageAccountName, config.StorageAccountKey)
+	if err != nil {
+		logger.Error("Cannot access storage account - wrong credentials", err)
+		return nil, api_error.NewInternalServerError("Cannot access storage account - wrong credentials", err)
+	}
+
+	serviceClient, err := azblob.NewServiceClientWithSharedKey(blobUrl.String(), cred, nil)
+
+	if err != nil {
+		logger.Error("Cannot access storage account - could not create service client", err)
+		return nil, api_error.NewInternalServerError("Cannot access storage account - could not create service client", err)
+	}
+	return &serviceClient, nil
+}
 
 func initRouter() {
 	gin.SetMode(config.GinMode)
@@ -27,6 +53,9 @@ func initRouter() {
 func wireApp() {
 	customerRepo := domain.NewJobRepositoryMem()
 	jobHandler = handler.JobHandlers{Service: service.NewJobService(customerRepo)}
+	azureFileRepo := domain.NewFileRepositoryAzure(azureClient)
+	_ = azureFileRepo
+	//fileService = service.NewFileService(azureFileRepo)
 }
 
 func startRouter() {
@@ -41,6 +70,10 @@ func startRouter() {
 func StartApp() {
 	logger.Info("Starting application")
 	err := config.InitConfig(config.EnvFile)
+	if err != nil {
+		panic(err)
+	}
+	azureClient, err = connectToAzureBlob()
 	if err != nil {
 		panic(err)
 	}

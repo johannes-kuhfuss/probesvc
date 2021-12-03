6937a21abaacfb4d015dcda18c2ec1ad1cd71a21
johannes-kuhfuss <johannes.kuhfuss@web.de>
2021-12-03 07:48:46 +0100

    Added FileRepo Mock

--- domain/File.go
+++ domain/File.go
@@ -2,6 +2,7 @@ package domain
 
 import "github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 
+//go:generate mockgen -destination=../mocks/domain/mockFileRepository.go -package=domain github.com/johannes-kuhfuss/probesvc/domain FileRepository
 type FileRepository interface {
 	GetClient() *azblob.ServiceClient
 }
